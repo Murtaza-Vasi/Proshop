b5e73b1d6e78db7cfa06cc16fb478db156a155cb
Murtaza Vasi <Murtaza-Vasi>
2021-01-31 17:40:35 +0530

    Integrated MongoDB

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import brcypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: brcypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: brcypt.hashSync('123456', 10)
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: brcypt.hashSync('123456', 10)
+  },
+]
+
+export default users;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config()
+connectDB();
 
 const app = express();
 
@@ -10,15 +14,10 @@ app.get('/', (req, res) => {
   res.send('API is running.... ')
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
