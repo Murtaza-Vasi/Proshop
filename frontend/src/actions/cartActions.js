2fec9d28f602d641810a9e32a1ed0e9e0dad396f
Murtaza Vasi <Murtaza-Vasi>
2021-02-06 12:56:10 +0530

    Added the add to cart and remove from cart functionality

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`)
+
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      countInStock: data.countInStock,
+      image: data.image,
+      price: data.price,
+      qty
+    }
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+
+}
