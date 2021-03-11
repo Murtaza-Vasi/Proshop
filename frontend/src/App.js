225b58e90a3e35c23941a3e30537c1fd97a664e3
Murtaza Vasi <Murtaza-Vasi>
2021-03-11 16:19:04 +0530

    Completed the frontend integration of user login/logout & update profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const App = () => {
   return (
@@ -15,6 +18,9 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,21 +1,36 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { 
+  userLoginReducer, 
+  userRegisterReducer, 
+  userDetailsReducer,
+  userUpdateProfileReducer 
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage
-  }
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage
+  },
 }
 
 const middleware = [thunk]
