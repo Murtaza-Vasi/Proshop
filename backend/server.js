a095e8f2e99e8d00622c781e8f9003a9edaf011c
Murtaza Vasi <Murtaza-Vasi>
2021-05-02 17:09:20 +0530

    created endpoints for adding user review

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
+
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
@@ -13,10 +15,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-app.get('/', (req, res) => {
-	res.send('API is running.... ');
-});
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
