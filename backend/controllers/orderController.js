cadd73cb8d7a4c181f2795efe6ce8d92e730c0c0
Murtaza Vasi <Murtaza-Vasi>
2021-05-02 15:00:44 +0530

    admin can now view all the orders placed and its details

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,35 +6,35 @@ import Order from '../models/orderModel.js';
 // @access  Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
-  const {
-    orderItems,
-    shippingAddress,
-    paymentMethod,
-    itemsPrice,
-    shippingPrice,
-    taxPrice,
-    totalPrice,
-  } = req.body;
+	const {
+		orderItems,
+		shippingAddress,
+		paymentMethod,
+		itemsPrice,
+		shippingPrice,
+		taxPrice,
+		totalPrice,
+	} = req.body;
 
-  if (orderItems && orderItems.length === 0) {
-    res.status(400);
-    throw new Error('No Order Items');
-  } else {
-    const order = new Order({
-      orderItems,
-      user: req.user._id,
-      shippingAddress,
-      paymentMethod,
-      itemsPrice,
-      shippingPrice,
-      taxPrice,
-      totalPrice,
-    });
+	if (orderItems && orderItems.length === 0) {
+		res.status(400);
+		throw new Error('No Order Items');
+	} else {
+		const order = new Order({
+			orderItems,
+			user: req.user._id,
+			shippingAddress,
+			paymentMethod,
+			itemsPrice,
+			shippingPrice,
+			taxPrice,
+			totalPrice,
+		});
 
-    const createdOrder = await order.save();
+		const createdOrder = await order.save();
 
-    res.status(201).json(createdOrder);
-  }
+		res.status(201).json(createdOrder);
+	}
 });
 
 // @desc    Get order by ID
@@ -42,17 +42,17 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id).populate(
-    'user',
-    'name email'
-  );
+	const order = await Order.findById(req.params.id).populate(
+		'user',
+		'name email'
+	);
 
-  if (order) {
-    res.json(order);
-  } else {
-    res.status(404);
-    throw new Error('Order not found');
-  }
+	if (order) {
+		res.json(order);
+	} else {
+		res.status(404);
+		throw new Error('Order not found');
+	}
 });
 
 // @desc    Update Order to Paid
@@ -60,26 +60,26 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @access  Private
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id);
+	const order = await Order.findById(req.params.id);
 
-  if (order) {
-    (order.isPaid = true), (order.paidAt = Date.now());
+	if (order) {
+		(order.isPaid = true), (order.paidAt = Date.now());
 
-    // This below paymentResult will be getting from PayPal
-    order.paymentResult = {
-      id: req.body.id,
-      status: req.body.status,
-      update_time: req.body.update_time,
-      email_address: req.body.payer.email_address,
-    };
+		// This below paymentResult will be getting from PayPal
+		order.paymentResult = {
+			id: req.body.id,
+			status: req.body.status,
+			update_time: req.body.update_time,
+			email_address: req.body.payer.email_address,
+		};
 
-    const updatedOrder = await order.save();
+		const updatedOrder = await order.save();
 
-    res.json(updatedOrder);
-  } else {
-    res.status(404);
-    throw new Error('Order not found');
-  }
+		res.json(updatedOrder);
+	} else {
+		res.status(404);
+		throw new Error('Order not found');
+	}
 });
 
 // @desc    Get logged In users orders
@@ -87,8 +87,23 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({ user: req.user._id });
-  res.json(orders);
+	const orders = await Order.find({ user: req.user._id });
+	res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+
+const getOrders = asyncHandler(async (req, res) => {
+	const orders = await Order.find({}).populate('user', 'id name');
+	res.json(orders);
+});
+
+export {
+	addOrderItems,
+	getOrderById,
+	updateOrderToPaid,
+	getMyOrders,
+	getOrders,
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import {
-  addOrderItems,
-  getOrderById,
-  updateOrderToPaid,
-  getMyOrders,
+	addOrderItems,
+	getOrderById,
+	updateOrderToPaid,
+	getMyOrders,
+	getOrders,
 } from '../controllers/orderController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(protect, addOrderItems);
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
