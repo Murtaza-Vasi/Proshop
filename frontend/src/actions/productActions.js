863c747868d1714c7b19152ce3762c2ad221ae66
Murtaza Vasi <Murtaza-Vasi>
2021-05-02 21:17:01 +0530

    users can now search for products from the homescreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,11 +21,11 @@ import {
 	PRODUCT_CREATE_REVIEW_FAIL,
 } from '../constants/productConstants';
 
-export const listProduct = () => async (dispatch) => {
+export const listProduct = (keyword = '') => async (dispatch) => {
 	try {
 		dispatch({ type: PRODUCT_LIST_REQUEST });
 
-		const { data } = await axios.get('/api/products');
+		const { data } = await axios.get(`/api/products?keyword=${keyword}`);
 
 		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
 	} catch (error) {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,39 +2,40 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 
-import Loader from '../components/Loader'
-import Message from '../components/Message'
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import Product from '../components/Product';
 import { listProduct } from '../actions/productActions';
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+	const keyword = match.params.keyword;
+	const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
 
-  const productList = useSelector(state => state.productList)
-  const { loading, error, products } = productList
+	useEffect(() => {
+		dispatch(listProduct(keyword));
+	}, [dispatch, keyword]);
 
-  useEffect(() => {
-    dispatch(listProduct())
-  }, [dispatch]);
+	return (
+		<>
+			<h1>Latest Products</h1>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
+		</>
+	);
+};
 
-
-  return (
-    <>
-      <h1>Latest Products</h1>
-      {loading ?
-        <Loader />
-        : error ?
-          <Message variant='danger'>{error}</Message>
-          : <Row>
-            {products.map(product => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>}
-    </>
-  )
-}
-
-export default HomeScreen
+export default HomeScreen;
