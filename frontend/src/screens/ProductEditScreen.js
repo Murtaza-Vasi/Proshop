e3c0e022bd25e076d23f7115f932024258149261
Murtaza Vasi <Murtaza-Vasi>
2021-05-02 14:21:45 +0530

    admin can now upload an image of the product while creating a new product

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
@@ -18,6 +20,7 @@ const ProductEditScreen = ({ match, history }) => {
 	const [category, setCategory] = useState('');
 	const [countInStock, setCountInStock] = useState('');
 	const [description, setDescription] = useState('');
+	const [uploading, setUploading] = useState(false);
 
 	const dispatch = useDispatch();
 
@@ -49,6 +52,28 @@ const ProductEditScreen = ({ match, history }) => {
 			}
 		}
 	}, [product, history, dispatch, productId, successUpdate]);
+
+	const uploadFileHandler = async (e) => {
+		const file = e.target.files[0];
+		const formData = new FormData();
+		formData.append('image', file);
+		setUploading(true);
+
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			};
+
+			const { data } = await axios.post('/api/upload', formData, config);
+			setImage(data);
+			setUploading(false);
+		} catch (error) {
+			console.error(error);
+			setUploading(false);
+		}
+	};
 
 	const submitHandler = (e) => {
 		e.preventDefault();
@@ -109,6 +134,13 @@ const ProductEditScreen = ({ match, history }) => {
 								value={image}
 								onChange={(e) => setImage(e.target.value)}
 							></Form.Control>
+							<Form.File
+								id='image-file'
+								label='Choose file'
+								custom
+								onChange={uploadFileHandler}
+							></Form.File>
+							{uploading && <Loader />}
 						</Form.Group>
 
 						<Form.Group controlId='brand'>
