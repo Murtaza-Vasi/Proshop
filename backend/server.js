0feb3787ba717e6f946e019b2d109f697ae49483
Murtaza Vasi <Murtaza-Vasi>
2021-05-05 14:54:01 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,14 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
