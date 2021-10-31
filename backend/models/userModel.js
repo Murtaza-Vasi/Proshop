f2d86f3c3ecdeb61f23e149ef5c52a4987d9a4ae
Murtaza Vasi <murtazavasi2000@gmail.com>
2021-10-31 18:13:02 +0530

    Minor bug fixes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,43 +1,45 @@
-import mongoose from 'mongoose'
-import brcypt from 'bcryptjs'
+import mongoose from 'mongoose';
+import brcypt from 'bcryptjs';
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false
-  }
-}, {
-  timestamps: true
-});
+const userSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await brcypt.compare(enteredPassword, this.password)
-}
+	return await brcypt.compare(enteredPassword, this.password);
+};
 
-userSchema.pre('save', async function(next) {
-  
-  if(!this.isModified('password')){
-    next()
-  }
-  
-  const salt = await brcypt.genSalt(10)
-  this.password = await brcypt.hash(this.password, salt)
-})
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next();
+	}
+
+	const salt = await brcypt.genSalt(10);
+	this.password = await brcypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
-export default User
+export default User;
