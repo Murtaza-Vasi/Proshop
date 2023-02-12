bac0c53fd6578230538aa6d82355df344ea3278e
Murtaza Vasi <murtazavasi2000@gmail.com>
2023-02-12 17:10:38 +0530

    Resolved some security issues

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+	try {
+		mongoose.set("strictQuery", true);
+		const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold);
-    process.exit(1);
-  }
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+	} catch (error) {
+		console.error(`Error: ${error.message}`.red.underline.bold);
+		process.exit(1);
+	}
 };
 
 export default connectDB;
